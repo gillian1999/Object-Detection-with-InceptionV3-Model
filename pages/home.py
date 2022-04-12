8915077f4c0235ea58e03b394a7330636bfda3c1
GeniusMpala <95400815+GeniusMpala@users.noreply.github.com>
2022-04-12 10:39:44 +0200

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -2,12 +2,12 @@
 import streamlit as st
 from Base_app import *
 
-files = [f for f in os.listdir("../en_images") if os.path.isfile(os.path.join("../en_images", f))]
+files = [f for f in os.listdir("./en_images") if os.path.isfile(os.path.join("./en_images", f))]
 fi = [i.split()[0] for i in files] 
 detected_obj = list(set(fi))
 
 def filter_frame(txt_search):
-    files = [f for f in os.listdir("../en_images ") if os.path.isfile(os.path.join("../en_images ", f))]
+    files = [f for f in os.listdir("./en_images ") if os.path.isfile(os.path.join("./en_images ", f))]
     files.sort(key = lambda x: int(x.split()[1][:-4]))
     indices = [i for i, s in enumerate(files) if txt_search.lower() in s]
     return files[indices[0]]
@@ -56,7 +56,7 @@ def app():
             st.write("")
            # if h_search.lower() in detected_obj:
             #    h_rez = "found"
-             #   img = Image.open('../en_images/'+str(filter_frame(h_search)))
+             #   img = Image.open('./en_images/'+str(filter_frame(h_search)))
               #  st.image(img, caption=h_search.lower() + " frame found")
             #else:
              #   play_video("video.mp4")
